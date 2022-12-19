27fef912c00e8ead77ff805344dc76f7fa208594
your_username <dannyclassic56@gmail.com>
2022-12-19 20:16:13 +0100

    responsive

--- app.js
+++ app.js
@@ -3,55 +3,102 @@ const low = document.querySelectorAll(".low");
 const li = document.querySelectorAll("li");
 const number = document.querySelector(".numbers");
 const contain = document.querySelector(".topB");
-const closes = document.querySelectorAll(".mainCLose");
+
 let gets = 0;
 const miniCont = document.querySelectorAll(".in");
 const body = document.querySelector("body");
 const topImage = document.querySelector(".topImage");
 const mode = document.querySelector(".mode");
-const check = document.querySelectorAll(".check");
+const bottom = document.querySelector(".bottom");
+const middle = document.querySelector(".middle");
+const adds = document.querySelector(".adds");
+let check = document.querySelectorAll(".check");
 
+let ticked = document.querySelectorAll(".ticked");
+let closes = document.querySelectorAll(".mainClose");
+let outs = document.querySelectorAll(".out");
+let outs2 = document.querySelectorAll(".out");
+let tickedArr = [];
+let checkArr = [];
+
+let closesArr = [];
+let outsArr = [];
+
+let eventing = true;
 const eve = function (eve) {
-  if (addToDo.value.length > 0 && eve.keyCode === 13) {
-    let p = document.createElement("p");
-    let div = document.createElement("div");
-    let inner = document.createElement("div");
-    let out = document.createElement("div");
-    let close = document.querySelector(".please");
-    close.classList.add("mainClose");
-    close.classList.remove("closed");
+  // console.log(contain);
+  const vavs = `
+  <div class="out">
+  <div class="in">
+    <div class="tick check"><p class="ticked">✓</p></div>
+    <p class="todo">${addToDo.value}</p>
+  </div>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="18"
+    height="18"
+    class="mainClose"
+   
+  >
+    <path
+      fill="currentcolor"
+      fill-rule="evenodd"
+      d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"
+      class="close"
+    />
+  </svg>
+  </div>`;
+  const addsUp = function () {
+    contain.insertAdjacentHTML("beforeend", vavs);
+  };
 
-    inner.classList.add("in");
-    out.classList.add("out");
-    p.appendChild(document.createTextNode(addToDo.value));
-    p.classList.add("todo");
-    div.classList.add("tick");
-    div.classList.add("check");
-    inner.appendChild(div);
-    inner.appendChild(p);
+  if (addToDo.value.length > 0) {
+    if (eve.keyCode === 13 || eve.target.id === "button") {
+      addsUp();
+      addToDo.value = "";
+      check = document.querySelectorAll(".check");
 
-    out.appendChild(inner);
-    out.appendChild(close);
-    contain.appendChild(out);
-    addToDo.value = "";
-    console.log(contain.length);
+      ticked = document.querySelectorAll(".ticked");
+      closes = document.querySelectorAll(".mainClose");
+      outs = document.querySelectorAll(".out");
+      tickedArr = [...ticked];
+      checkArr = [...check];
 
-    gets += 1;
-    number.textContent = `${gets}  `;
-
-    div.addEventListener("click", function () {
-      console.log("ahhhhhhhhss");
-      div.classList.toggle("checked");
-      div.textContent = "✓";
-    });
+      closesArr = [...closes];
+      outsArr = [...outs];
+    }
   }
 };
 addToDo.addEventListener("keydown", eve);
+adds.addEventListener("click", eve);
+function works() {
+  checkArr.forEach(function (check, i) {
+    check.addEventListener("click", function () {
+      check.classList.toggle("checked");
+      tickedArr[i].classList.toggle("✅");
+    });
+  });
+
+  closesArr.forEach(function (close, i) {
+    close.addEventListener("click", function () {
+      outsArr[i].remove();
+    });
+  });
+  low[1].addEventListener("click", function () {
+    outsArr.forEach(function (outs, i) {
+      outs.remove();
+    });
+  });
+}
+
+setInterval(works, 100);
 
 mode.addEventListener("click", function () {
   mode.classList.toggle("dim");
   body.classList.toggle("white");
   topImage.classList.toggle("dark");
+  bottom.classList.toggle("light");
+  middle.classList.toggle("light");
 });
 
 for (const lows of low) {
@@ -65,3 +112,10 @@ for (const lis of li) {
     lis.classList.toggle("lis");
   });
 }
+
+const items = function () {
+  outs2 = document.querySelectorAll(".out");
+  low[0].innerHTML = `${outs2.length} item(s) left.`;
+};
+
+setInterval(items, 100);
